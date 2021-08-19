b9fb2816f1b8ad3074847d1fe86ab9dcd6a85587
hasanbolatt <87964820+hasanbolatt@users.noreply.github.com>
2021-08-19 10:26:10 +0300

    sayfa düzenlendi

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const express = require('express');
+const ejs = require('ejs');2
 const path = require('path');
 
 const app = express();
 
+//template engine 
+app.set("view engine","ejs");
+
 //Middleware response ile request arasındaki oluşan olaylar bütünü
 //Middleware fonksiyonu 
 //Middlewarelar sırayla çalışır
-
 //İlgili middleware fonksiyonunu yazarak public klasörümüzü uygulamamıza kaydedelim.
 app.use(express.static('public'))    
 
+//routes
 app.get('/', (req, res) => {
   
-  res.sendFile(path.resolve(__dirname,'temp/index.html'));
+  //res.sendFile(path.resolve(__dirname,'temp/index.html'));
+  res.render('index');
+})
+
+app.get('/about', (req, res) => {
+  
+  //res.sendFile(path.resolve(__dirname,'temp/index.html'));
+  res.render('about');
+})
+
+app.get('/add', (req, res) => {
+  
+  //res.sendFile(path.resolve(__dirname,'temp/index.html'));
+  res.render('add');
 })
 
 const port = 3000;
